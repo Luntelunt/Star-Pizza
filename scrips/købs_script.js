2069063ff20198b800e025e468e4c6b0bec38338
Luntelunt <line.mh@hotmail.com>
2025-03-24 00:15:18 +0100

    Add files via upload

---
+++ scrips/købs_script.js
@@ -0,0 +1,20 @@
+function klikKob(){
+if (sessionStorage.clickcount) {
+    sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+} else
+{
+    sessionStorage.clickcount = 1;
+}
+document.getElementById("kurvAntal").innerHTML = sessionStorage.clickcount;
+document.getElementById("endePris").innerHTML = sessionStorage.clickcount * 120;
+}
+
+function fjernKob(){
+    if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount) - 1;
+    } else
+    {
+        sessionStorage.clickcount = 1;
+    }
+    document.getElementById("kurvAntal").innerHTML = sessionStorage.clickcount;
+    }
